93a2c2fecef2d87034bf712c04bca7ae7a3c7e86
Yash <88078210+yash0903@users.noreply.github.com>
2022-06-16 22:31:06 +0530

    Time: 3 ms (60.52%), Space: 57.6 MB (6.81%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -4,25 +4,3 @@ class Solution {
         return nums[nums.length/2];
     }
 }
-
-// class Solution {
-//     // Moore's sorting algo
-//     public int majorityElement(int[] nums) {
-//         int val = nums[0];
-//         int count = 1;
-//         int i = 1;
-//         while(i < nums.length){
-//             if(nums[i] == val){
-//                 count++;
-//             } else{
-//                 count--;
-//                 if(count == 0){
-//                     val = nums[i];
-//                     count = 1;
-//                 }
-//             }
-//             i++;
-//         }
-//         return val;
-//     }
-// }
