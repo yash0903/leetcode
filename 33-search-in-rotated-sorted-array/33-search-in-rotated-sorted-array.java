d2b400aec8eaa9ac3525092152f1d5c2ad64b97d
Yash <88078210+yash0903@users.noreply.github.com>
2022-06-27 23:10:43 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (30.61%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        // brute force approach
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == target){
+                return i;
+            }
+        }
+        return - 1;
+    }
+}
