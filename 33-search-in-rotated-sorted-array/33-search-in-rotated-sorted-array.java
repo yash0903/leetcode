3949cf0611736704b8399face197265c50e2b31a
Yash <88078210+yash0903@users.noreply.github.com>
2022-07-19 21:22:51 +0530

    Time: 1 ms (74.47%), Space: 42.7 MB (46.43%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,11 +1,53 @@
 class Solution {
     public int search(int[] nums, int target) {
-        // brute force approach
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == target){
-                return i;
+        int pivot = 0;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i]< nums[i-1]){
+                pivot = i-1;
             }
         }
-        return - 1;
+        
+        int a = searchInRight(nums, pivot, target);
+        int b = searchInLeft(nums, pivot, target);
+        
+        if(a == -1){
+            return b;
+        }else{
+            return a;
+        }
+    }
+    
+    int searchInRight(int[] nums, int pivot, int target){
+        int s = pivot+1;
+        int e = nums.length-1;
+        
+        while(s<=e){
+            int m = s+(e-s)/2;
+            if(nums[m] == target){
+                return m;
+            }else if(target > nums[m]){
+                s = m + 1;
+            }else{
+                e = m - 1;
+            }
+        }
+        return -1;
+    }
+    
+    int searchInLeft(int[] nums, int pivot, int target){
+        int s = 0;
+        int e = pivot;
+        
+        while(s <= e){
+            int m = s+(e-s)/2;
+            if(nums[m] == target){
+                return m;
+            }else if(target>nums[m]){
+                s = m + 1;
+            }else{
+                e = m - 1;
+            }
+        }
+        return -1;
     }
 }
