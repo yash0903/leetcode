8ace789ea94e0c63968cf2ab675c4d6a86da83cf
Yash <88078210+yash0903@users.noreply.github.com>
2022-07-24 13:55:04 +0530

    Time: 1 ms (38.49%), Space: 47 MB (65.39%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,44 +1,41 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        //optimal solution
-        int a = first(nums,target);
-        int b = second(nums,target);
-        
+        int a = searchInLeft(nums,target);
+        int b = searchInRight(nums,target);
         return new int[] {a,b};
     }
     
-    int first(int[] arr, int target){
+    int searchInLeft(int[] nums, int target){
         int s = 0;
-        int e =arr.length-1;
+        int e = nums.length-1;
         int r = -1;
-        
         while(s <= e){
             int m = s + (e-s)/2;
-            if(target > arr[m]){
-                s = m + 1;
-            }else if (target < arr[m]){
-                e = m - 1;
-            }else {
+            if(nums[m] == target){
                 r = m;
-                e = m - 1;
+                e = m-1;
+            }else if(nums[m] > target){
+                e = m -1;
+            }else{
+                s = m + 1;
             }
         }
         return r;
     }
     
-    int second(int[] arr, int target){
-        int s = 0;
-        int e = arr.length-1;
+    int searchInRight(int[] nums, int target){
+        int s  = 0;
+        int e = nums.length-1;
         int r = -1;
         
         while(s <= e){
             int m = s + (e-s)/2;
-            if(target > arr[m]){
-                s = m + 1;
-            }else if(target < arr[m]){
-                e = m - 1;
-            }else {
+            if(nums[m] == target){
                 r = m;
+                s = m + 1;
+            }else if(nums[m] > target){
+                e = m - 1;
+            }else{
                 s = m + 1;
             }
         }
